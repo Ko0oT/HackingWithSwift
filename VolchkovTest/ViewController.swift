38227795da19da2a3ac22a6872afed6c3b346f92
Сергей Волчков <serkokarjalo@gmail.com>
2025-03-18 10:32:08 +0300

    part 1

--- VolchkovTest/ViewController.swift
+++ VolchkovTest/ViewController.swift
@@ -10,14 +10,27 @@ import Stevia
 
 final class ViewController: UIViewController {
     
-  
+    //MARK: - Propereries
+    var pictures = [String]()
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
         
+        //Константа items будет массивом строк, содержащих имена файлов.
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+            
+        }
+        print(pictures)
     }
-    
 }
 
 
