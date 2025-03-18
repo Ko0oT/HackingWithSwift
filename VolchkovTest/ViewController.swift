d1e6096d6f0d19e937164cf68427c68846f27e91
Сергей Волчков <serkokarjalo@gmail.com>
2025-03-18 09:23:19 +0300

    Initial Commit

---
+++ VolchkovTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  VolchkovTest
+//
+//  Created by Citylink on 25.01.2025.
+//
+
+import UIKit
+import Stevia
+
+final class ViewController: UIViewController {
+    
+  
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
+
+
